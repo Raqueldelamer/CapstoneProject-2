f0b74d83b60c621e2cab0d08ce0764849d46b5fb
Raquel Marie <raqueldelamer@gmail.com>
2024-09-09 21:21:50 -0500

    update readme

---
+++ script.js
@@ -0,0 +1,31 @@
+let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=qdKmIGSnIsK5iNoMCvpbQwkaWZuXIclEzQNe1E35`; //api_key entered
+
+let fetchButton = document.getElementById('fetch-button');
+let apodImage = document.getElementById('apod-image');
+let apodTitle = document.getElementById('apod-title');
+let apodDescription = document.getElementById('apod-description');
+console.log(apiUrl);
+
+
+async function fetchImage() {
+    try {
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+        console.log(data);
+
+        
+        apodImage.src = data.url;
+        apodImage.alt = data.title;
+        apodTitle.textContent = data.title;
+        apodDescription.textContent = data.explanation;
+        console.log(apodImage.alt);
+
+    } catch (error) {
+        console.error('Error fetching image data:', error);
+        apodImage.src = '';
+        apodTitle.textContent = 'Error';
+        
+    }
+}
+
+fetchButton.addEventListener('click', fetchImage);
